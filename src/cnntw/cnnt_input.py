105a1ea202b25ed59e409e95f7b68c6c7fadc5a0
Bonggun Shin <nomolos79@gmail.com>
2017-03-16 01:43:34 -0400

    trim cnnt input test

--- src/cnntw/cnnt_input.py
+++ src/cnntw/cnnt_input.py
@@ -1,20 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Routine for decoding the CIFAR-10 binary file format."""
-
+"""Routine for decoding the cnnt binary file format."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -36,74 +20,25 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
 
 
 def read_cnnt(filename_queue):
-  """Reads and parses examples from CIFAR10 data files.
+    class CNNTRecord(object):
+        pass
+    result = CNNTRecord()
 
-  Recommendation: if you want N-way read parallelism, call this function
-  N times.  This will give you N independent Readers reading different
-  files & positions within those files, which will give better mixing of
-  examples.
+    label_bytes = 1*8
+    result.maxlen = 60
+    result.w2vdim = 400
+    input_bytes = (result.maxlen * result.w2vdim)*8
+    record_bytes = label_bytes + input_bytes
 
-  Args:
-    filename_queue: A queue of strings with the filenames to read from.
+    reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+    result.key, value = reader.read(filename_queue)
 
-  Returns:
-    An object representing a single example, with the following fields:
-      height: number of rows in the result (32)
-      width: number of columns in the result (32)
-      depth: number of color channels in the result (3)
-      key: a scalar string Tensor describing the filename & record number
-        for this example.
-      label: an int32 Tensor with the label in the range 0..9.
-      uint8image: a [height, width, depth] uint8 Tensor with the image data
-  """
+    value_as_floats = tf.decode_raw(value, tf.float64)
+    result.label = tf.cast(value_as_floats[0], tf.int32)
 
-  class CNNTRecord(object):
-    pass
-  result = CNNTRecord()
+    features = value_as_floats[1:1 + record_bytes]
+    result.features = tf.reshape(features, [result.maxlen, result.w2vdim])
 
-  # Dimensions of the images in the CIFAR-10 dataset.
-  # See http://www.cs.toronto.edu/~kriz/cifar.html for a description of the
-  # input format.
-  label_bytes = 1*8  # 2 for CIFAR-100
-  result.maxlen = 60
-  result.w2vdim = 400
-  input_bytes = (result.maxlen * result.w2vdim)*8
-  # Every record consists of a label followed by the image, with a
-  # fixed number of bytes for each.
-  record_bytes = label_bytes + input_bytes
-
-  # Read a record, getting filenames from the filename_queue.  No
-  # header or footer in the CIFAR-10 format, so we leave header_bytes
-  # and footer_bytes at their default of 0.
-  reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
-  result.key, value = reader.read(filename_queue)
-
-  # Convert from a string to a vector of uint8 that is record_bytes long.
-  # record_bytes = tf.decode_raw(value, tf.float32)
-
-  # Decode the result from bytes to int32
-  # record_bytes = tf.decode_raw(value, tf.int32, little_endian=True)
-  value_as_floats = tf.decode_raw(value, tf.float64)
-  result.label = value_as_floats[0]
-
-  # Decode the result from bytes to float32
-  # value_as_floats = tf.decode_raw(value, tf.float32)
-  features = value_as_floats[1:1 + record_bytes]
-  result.features = tf.reshape(features, [result.maxlen, result.w2vdim])
+    return result
 
 
-  #
-  # # The first bytes represent the label, which we convert from uint8->int32.
-  # result.label = tf.cast(
-  #     tf.strided_slice(record_bytes, [0], [label_bytes]), tf.float32)
-
-  # The remaining bytes after the label represent the image, which we reshape
-  # from [depth * height * width] to [depth, height, width].
-  # result.depth_major = tf.reshape(
-  #     tf.strided_slice(record_bytes, [label_bytes],
-  #                      [label_bytes + input_bytes], tf.float32),
-  #     [result.maxlen, result.w2vdim])
-  # Convert from [depth, height, width] to [height, width, depth].
-  # result.uint8image = tf.transpose(depth_major, [1, 2, 0])
-
-  return result
--- src/cnntw/cnnt_input_test.py
+++ src/cnntw/cnnt_input_test.py
@@ -1,20 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Tests for cifar10 input."""
-
+"""Tests for cnnt input."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -28,43 +12,44 @@ import numpy as np
 
 
 class CNNTInputTest(tf.test.TestCase):
+    def _record(self, label, num):
+        input_size = 60 * 400
+        x = np.ones([60,400]) * num
+        x = x.reshape([input_size])
+        x = np.append(float(label), x)
+        # x = np.array([label] + [num] * input_size)
+        record = x.tostring()
+        expected = np.array([[[num]] * 400] * 60)
+        return record, expected[:,:,0]
 
-  def _record(self, label, num):
-    input_size = 60 * 400
-    x = np.ones([60,400]) * num
-    x = x.reshape([input_size])
-    x = np.append(float(label), x)
-    # x = np.array([label] + [num] * input_size)
-    record = x.tostring()
-    expected = np.array([[[num]] * 400] * 60)
-    return record, expected[:,:,0]
+    def testSimple(self):
+        labels = [1, 0, 2]
+        records = [self._record(labels[0], 0.1),
+                   self._record(labels[1], 0.2),
+                   self._record(labels[2], 0.3)]
+        contents = b"".join([record for record, _ in records])
+        expected = [expected for _, expected in records]
+        filename = os.path.join(self.get_temp_dir(), "cnnt")
+        open(filename, "wb").write(contents)
 
-  def testSimple(self):
-    labels = [1, 0, 2]
-    records = [self._record(labels[0], 0.1),
-               self._record(labels[1], 0.2),
-               self._record(labels[2], 0.3)]
-    contents = b"".join([record for record, _ in records])
-    expected = [expected for _, expected in records]
-    filename = os.path.join(self.get_temp_dir(), "cnnt")
-    open(filename, "wb").write(contents)
+        with self.test_session() as sess:
+            q = tf.train.string_input_producer([filename])
+            q.enqueue([filename]).run()
+            q.close().run()
+            result = cnnt_input.read_cnnt(q)
 
-    with self.test_session() as sess:
-      q = tf.train.string_input_producer([filename])
-      q.enqueue([filename]).run()
-      q.close().run()
-      result = cnnt_input.read_cnnt(q)
+            for i in range(3):
+                key, label, features = sess.run([
+                    result.key, result.label, result.features])
+                self.assertEqual("%s:%d" % (filename, i), tf.compat.as_text(key))
+                self.assertEqual(labels[i], label)
+                self.assertAllEqual(expected[i], features)
 
-      for i in range(3):
-        key, label, features = sess.run([
-            result.key, result.label, result.features])
-        self.assertEqual("%s:%d" % (filename, i), tf.compat.as_text(key))
-        self.assertEqual(labels[i], label)
-        self.assertAllEqual(expected[i], features)
-
-      with self.assertRaises(tf.errors.OutOfRangeError):
-        sess.run([result.key, result.features])
-
+            with self.assertRaises(tf.errors.OutOfRangeError):
+                sess.run([result.key, result.features])
 
 if __name__ == "__main__":
-  tf.test.main()
+    tf.test.main()
+
+
+
