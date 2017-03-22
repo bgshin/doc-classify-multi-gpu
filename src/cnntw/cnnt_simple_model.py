9cef325e07a046a87759aa720c232e758c392828
Bonggun Shin <nomolos79@gmail.com>
2017-03-22 17:03:04 -0400

    .

--- src/cnntw/cnnt_simple_model.py
+++ src/cnntw/cnnt_simple_model.py
@@ -21,7 +21,7 @@ tf.app.flags.DEFINE_string('train_dir', './train/',
                            """and checkpoint.""")
 tf.app.flags.DEFINE_integer('max_steps', 1000000,
                             """Number of batches to run.""")
-tf.app.flags.DEFINE_integer('num_gpus', 2,
+tf.app.flags.DEFINE_integer('num_gpus', 4,
                             """How many GPUs to use.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
@@ -198,8 +198,8 @@ def train():
         # Start running operations on the Graph. allow_soft_placement must be set to
         # True to build towers on GPU, as some of the ops do not have GPU
         # implementations.
-        gpu_options = tf.GPUOptions(visible_device_list=str('0,1,2,3'), allow_growth=True)
-        # gpu_options = tf.GPUOptions(visible_device_list=str('2,3'), allow_growth=True)
+        # gpu_options = tf.GPUOptions(visible_device_list=str('0,1,2,3'), allow_growth=True)
+        gpu_options = tf.GPUOptions(visible_device_list=str('2,3'), allow_growth=True)
         sess = tf.Session(config=tf.ConfigProto(
             gpu_options=gpu_options,
             allow_soft_placement=True,
